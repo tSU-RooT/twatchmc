c8c70cd9cb6986ff811c2d6446b967a457caa283
tSURooT <961pro@gmail.com>
2015-03-14 01:55:12 +0900

    テストを追加(書きかけ)

---
+++ structs_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+  "time"
+)
+
+func TestDeathCountUp(t *testing.T) {
+	player := PlayerData{Name: "User", DeathCount: 0, KillCount: 0,
+		DeathHistory: make([]Death, 0), KilledTable: make(map[string]int, 0)}
+  death1 := Death{ID: 10, Type: 0, Timestamp: time.Now(), KilledBy: "", KilledByOtherPlayer: false}
+  event, exist := player.DeathCountUp(death1)
+  if event != "" || exist {
+    t.Fatal("Return Message should be empty")
+  }
+  for i:=0;i<9;i++ {
+    event, exist = player.DeathCountUp(death1)
+  }
+  if !(event == (player.Name + "は 通算10回 死亡した。") && exist) {
+    t.Fatal("Return Message should be \"" + player.Name + "は 通算10回 死亡した。\"")
+  }
+}
